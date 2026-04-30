95172fbd28f84321565b95971d89dc2710592724
Shunichi Ikegami <sike.tm@gmail.com>
2026-04-30 17:18:32 +0900

    テストのレポート出力

--- packages/server/vitest.config.e2e.ts
+++ packages/server/vitest.config.e2e.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
 
-export default defineConfig({
-  resolve: {
-    tsconfigPaths: true,
-  },
-  test: {
-    globalSetup: ["tests/global-setup.e2e.ts"],
-    include: ["../core/tests/api/**/*.test.ts"],
-    pool: "forks",
-    maxWorkers: 1,
-    isolate: false,
-    env: { TEST_PORT: "12346" },
-    alias: {
-      "tests/": new URL("../core/tests/", import.meta.url).pathname,
+import sharedConfig from "../../vitest.shared";
+
+export default mergeConfig(
+  sharedConfig,
+  defineConfig({
+    resolve: {
+      tsconfigPaths: true,
     },
-  },
-});
+    test: {
+      globalSetup: ["tests/global-setup.e2e.ts"],
+      include: ["../core/tests/api/**/*.test.ts"],
+      pool: "forks",
+      maxWorkers: 1,
+      isolate: false,
+      env: { TEST_PORT: "12346" },
+      alias: {
+        "tests/": new URL("../core/tests/", import.meta.url).pathname,
+      },
+    },
+  }),
+);
